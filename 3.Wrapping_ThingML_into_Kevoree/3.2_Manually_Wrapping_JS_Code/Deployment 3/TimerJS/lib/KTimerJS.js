1fcf5663153ad5585b3bc3ecf04f4e773910a805
Maxime Tricoire <max.tricoire@gmail.com>
2015-01-22 11:45:29 +0100

    clean Kevoree comp

--- 3.Wrapping_ThingML_into_Kevoree/3.2_Manually_Wrapping_JS_Code/Deployment 3/TimerJS/lib/KTimerJS.js
+++ 3.Wrapping_ThingML_into_Kevoree/3.2_Manually_Wrapping_JS_Code/Deployment 3/TimerJS/lib/KTimerJS.js
@@ -1,5 +1,4 @@
 var TimerJS = require('./TimerJS');
-var Connector = require('./Connector');
 var AbstractComponent = require('kevoree-entities').AbstractComponent;
 
 /**
@@ -9,16 +8,9 @@ var AbstractComponent = require('kevoree-entities').AbstractComponent;
 var KTimerJS = AbstractComponent.extend({
     toString: 'KTimerJS',
 
-	//TODO: update of Kevoree attributes should be propagated to things
-    /* This is an example of dictionary attribute that you can set for your entity */
-    //dic_yourAttrName: {
-    //  optional: true,
-    //  defaultValue: false
-    //},
-
 	//ADDED: Reference to the wrapped things
 	construct: function() {
-		this.TestTimerJS_timer = null;
+		this.timer = null;
 	},
 
     /**
@@ -27,18 +19,16 @@ var KTimerJS = AbstractComponent.extend({
      */
     start: function (done) {
         this._super(function () {
-		this.log.debug(this.toString(), 'STARTING...');
 		//ADDED: Main (instantiation of things and connectors) from the generated code
 		//TODO: instances should be accessed with this.X
-		this.TestTimerJS_timer = new TimerJS();
-		this.TestTimerJS_timer.setThis(this.TestTimerJS_timer);
+		this.timer = new TimerJS();
+		this.timer.setThis(this.timer);
 
 		//register listeners to allow wrapped things to send on Kevoree ports
-		this.TestTimerJS_timer.getTimerListeners().push(this.out_TestTimerJS_timer_timer_out.bind(this));
+		this.timer.getTimerListeners().push(this.out_TestTimerJS_timer_timer_out.bind(this));
 
-		this.TestTimerJS_timer._init();
+		this.timer._init();
 
-		this.log.debug(this.toString(), 'STARTED!');
             done();
         }.bind(this));
     },
@@ -50,9 +40,7 @@ var KTimerJS = AbstractComponent.extend({
     stop: function (done) {
         this._super(function () {
 			//ADDED: On SIG-INT hook
-            this.log.debug(this.toString(), 'STOPPING!');
-			this.TestTimerJS_timer._stop();
-            this.log.debug(this.toString(), 'STOPPED!');
+			this.timer._stop();
             done();
         }.bind(this));
     },
@@ -63,9 +51,9 @@ var KTimerJS = AbstractComponent.extend({
 		var json = JSON.parse(msg);
 		if (json.port === "timer_c") {
 			if (json.message === "timer_start") {
-				this.TestTimerJS_timer.receivetimer_startOntimer(json.delay);
+				this.timer.receivetimer_startOntimer(json.delay);
 			} else if (json.message === "timer_cancel") {
-				this.TestTimerJS_timer.receivetimer_cancelOntimer();
+				this.timer.receivetimer_cancelOntimer();
 			}
 		} //else another port
     },
