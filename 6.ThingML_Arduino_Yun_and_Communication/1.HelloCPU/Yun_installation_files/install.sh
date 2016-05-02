6365145d865191d6d0274387a182151b5dcf965a
Nicolas Harrand <nicolas.harrand@gmail.com>
2016-05-02 19:23:14 +0200

    Update install.sh

--- 6.ThingML_Arduino_Yun_and_Communication/1.HelloCPU/Yun_installation_files/install.sh
+++ 6.ThingML_Arduino_Yun_and_Communication/1.HelloCPU/Yun_installation_files/install.sh
@@ -13,7 +13,5 @@ echo "Shutting Bridge down"
 
 sed -i'' '/ttyATH0/ s/^/#/' /etc/inittab
 
-echo "In order to set it up, uncomment the line containing /ttyATH0 in the file /etc/inittab"
-
 echo "You can now reboot the CPU of your Yùn"
 
